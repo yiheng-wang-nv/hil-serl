b58ce34194d0a48dbb57c28f4445867cefd5a5ff
Bin Liu (SW-GPU) <binliu@nvidia.com>
2025-10-29 17:44:41 +0800

    add env install

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ HIL-SERL provides a set of libraries, env wrappers, and examples to train RL pol
 
     - For GPU:
         ```bash
-        pip install --upgrade "jax[cuda12_pip]==0.4.35" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+        pip install --upgrade "jax[cuda12_pip]==0.4.35" -i https://pypi.tuna.tsinghua.edu.cn/simple
         ```
 
     - For TPU
---
+++ install_g1_env.sh
@@ -0,0 +1,26 @@
+## Installation
+1. **Setup Conda Environment:**
+    create an environment with
+    ```bash
+    conda create -n hilserl python=3.10
+    ```
+
+2. **Install Jax as follows:**
+
+```
+pip install --upgrade "jax[cuda12_pip]==0.4.35" -i https://pypi.tuna.tsinghua.edu.cn/simple
+```
+
+3. **Install the serl_launcher**
+    ```bash
+    cd serl_launcher
+    pip install -e .
+    pip install -r requirements.txt
+    ```
+
+4. **Install Unitree Dependencies**
+
+```bash
+cd unitree_sdk2_python
+pip install -e .
+```
